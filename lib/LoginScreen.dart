717116707847a8695affecbd6ff06ef8f4ec01e9
BluseSara <sara.alluhaibi@outlook.com>
2024-12-22 23:47:23 +0300

    first commit

---
+++ lib/LoginScreen.dart
@@ -0,0 +1,31 @@
+import 'package:bookbloom/ShapesClasses/LoginShape.dart';
+import 'package:bookbloom/ShapesClasses/SplachShape.dart';
+import 'package:flutter/material.dart';
+
+
+class Loginscreen extends StatelessWidget {
+  const Loginscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Transform.translate(
+                offset: const Offset(40, 30), //المسافات
+              child: CustomPaint(
+                // رسمة login
+
+                size: const Size(307, 231),
+                painter: RPSSCustomPainter(),
+
+
+              ),
+            ),
+          ],
+        ),
+      ),
+            );
+  }
+}
---
+++ lib/SplachScreen.dart
@@ -0,0 +1,28 @@
+import 'package:bookbloom/ShapesClasses/SplachShape.dart';
+import 'package:flutter/material.dart';
+
+
+class Splachscreen extends StatelessWidget {
+  const Splachscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Transform.translate(
+              offset: const Offset(-30, 50), //المسافات
+              child: CustomPaint(
+                // رسمة splash
+
+                size: const Size(354, 401),
+                painter: RPSCustomPainter(),
+              ),
+            ),
+          ],
+        ),
+      ),
+            );
+  }
+}
