f56c2ddefc60947ce2b879afa8795ad5584a7222
SARA ALLUHAIBI <sara.alluhaibi@outlook.com>
2024-12-26 13:14:49 +0300

    Update LoginScreen.dart

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,5 +1,38 @@
-import 'package:bookbloom/ShapesClasses/LoginShape.dart';
 import 'package:flutter/material.dart';
+import 'package:bookbloom/BaseClasses/ColorClass.dart'; // استيراد ColorClass
+import 'package:bookbloom/BaseClasses/TextClass.dart'; // استيراد TextClass
+import 'package:bookbloom/ShapesClasses/LoginShape.dart'; // استيراد LoginShape
+
+class TextStyles {
+  //Welcome to .. End
+  static const FirstBig = TextStyle(
+    fontSize: 94.6,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Roboto',
+    color: Colorclass.brown,
+  );
+  //BookBloom
+  static const FirstBig2 = TextStyle(
+    fontSize: 94.6,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Roboto',
+    color: Colorclass.dustyPink,
+  );
+
+  static const FirstMed = TextStyle(
+    fontSize: 45.9,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'DMSans',
+    color: Colorclass.gbrown,
+  );
+
+  static const inButton = TextStyle(
+    fontSize: 45.9,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'DMSans',
+    color: Colorclass.gbrown,
+  );
+}
 
 class Loginscreen extends StatelessWidget {
   const Loginscreen({super.key});
@@ -11,12 +44,17 @@ class Loginscreen extends StatelessWidget {
         child: Column(
           children: [
             Transform.translate(
-              offset: const Offset(40, 30), //المسافات
+              offset: const Offset(40, 30), // المسافات
               child: CustomPaint(
                 // رسمة login
                 size: const Size(307, 231),
                 painter: RPSSCustomPainter(),
               ),
+            ),
+            // استدعاء الكلمة "LOGIN" من Textclass واستخدام النمط FirstMed من TextStyles
+            Text(
+              Textclass.Login, // استخدام Login من Textclass
+              style: TextStyles.FirstMed, // استخدام FirstMed من TextStyles
             ),
           ],
         ),
