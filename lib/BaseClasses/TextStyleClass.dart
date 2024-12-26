0bf306724d6ce7e3b556bf8023e3c01cda24db57
SARA ALLUHAIBI <sara.alluhaibi@outlook.com>
2024-12-26 13:12:55 +0300

    Update TextStyleClass.dart

---
+++ lib/BaseClasses/TextStyleClass.dart
@@ -0,0 +1,31 @@
+import 'package:bookbloom/BaseClasses/ColorClass.dart';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  //Welcome to .. End
+  static const FirstBig = TextStyle(
+      fontSize: 94.6,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Roboto',
+      color: Colorclass.brown);
+  //BookBloom
+  static const FirstBig2 = TextStyle(
+      fontSize: 94.6,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Roboto',
+      color: Colorclass.dustyPink);
+
+  static const FirstMed = TextStyle(
+    fontSize: 45.9,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'DMSans',
+    color: Colorclass.gbrown,
+  );
+
+  static const inBoutton = TextStyle(
+    fontSize: 45.9,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'DMSans',
+    color: Colorclass.gbrown,
+  );
+}
