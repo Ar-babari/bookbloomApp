af95989b4bffa938b8d49112ab1a75fcdee7653f
SARA ALLUHAIBI <sara.alluhaibi@outlook.com>
2024-12-26 13:25:18 +0300

    Update LoginScreen.dart

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,39 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:bookbloom/BaseClasses/ColorClass.dart'; // استيراد ColorClass
-import 'package:bookbloom/BaseClasses/TextClass.dart'; // استيراد TextClass
-import 'package:bookbloom/ShapesClasses/LoginShape.dart'; // استيراد LoginShape
-
-class TextStyles {
-  //Welcome to .. End
-  static const FirstBig = TextStyle(
-    fontSize: 94.6,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Roboto',
-    color: Colorclass.brown,
-  );
-  //BookBloom
-  static const FirstBig2 = TextStyle(
-    fontSize: 94.6,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Roboto',
-    color: Colorclass.dustyPink,
-  );
-
-  static const FirstMed = TextStyle(
-    fontSize: 45.9,
-    fontWeight: FontWeight.normal,
-    fontFamily: 'DMSans',
-    color: Colorclass.gbrown,
-  );
-
-  static const inButton = TextStyle(
-    fontSize: 45.9,
-    fontWeight: FontWeight.normal,
-    fontFamily: 'DMSans',
-    color: Colorclass.gbrown,
-  );
-}
-
 class Loginscreen extends StatelessWidget {
   const Loginscreen({super.key});
 
