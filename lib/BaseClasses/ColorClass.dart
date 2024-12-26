adcf5bba8870c1491cff9c5dd9b15fd2dee1d664
SARA ALLUHAIBI <sara.alluhaibi@outlook.com>
2024-12-26 12:54:37 +0300

    Update ColorClass.dart

--- lib/BaseClasses/ColorClass.dart
+++ lib/BaseClasses/ColorClass.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Colorclass {
-  static const Color dustyPink = Color.fromARGB(255, 213, 173, 166); // #d5ada6
+  static const Color dustyPink = Color.fromARGB(255, 213, 173, 166); 
   static const Color brown = Color.fromARGB(255, 50, 33, 30);   
 
+
+// حق الايقونات ابو لونين
   static const LinearGradient gradient = LinearGradient(
     colors: [dustyPink, brown],
     begin: Alignment.bottomRight,
     end: Alignment.bottomLeft,
     );
+
+  
+    static const gbrown  =   Color.fromARGB(255, 149, 133, 130);
+    static  const grey = Color.fromARGB(255, 219, 219, 219);
 }
