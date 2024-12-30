0e44d4b09acec6eb8264062ff6421bb2741157a7
BluseSara <sara.alluhaibi@outlook.com>
2024-12-30 19:42:14 +0300

    ز

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:bookbloom/BaseClasses/ColorClass.dart';
+import 'package:bookbloom/BaseClasses/TextClass.dart';
 import 'package:bookbloom/BaseClasses/TextStyleClass.dart';
+import 'package:bookbloom/ShapesClasses/LoginShape.dart'; // كلاس الرسمة
 import 'package:flutter/material.dart';
-import 'package:bookbloom/BaseClasses/ColorClass.dart'; // استيراد ColorClass
-import 'package:bookbloom/ShapesClasses/LoginShape.dart';
 
 class Loginscreen extends StatelessWidget {
   const Loginscreen({super.key});
@@ -15,10 +18,151 @@ class Loginscreen extends StatelessWidget {
           children: [
             // الرسمة في الأعلى
             Transform.translate(
-              offset: const Offset(40, 30), // المسافات
+              offset: const Offset(30, 7), // المسافات للرسمة
               child: CustomPaint(
                 size: const Size(307, 231),
                 painter: RPSSCustomPainter(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // العنوان والنصوص على اليسار
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Align(
+                alignment: Alignment.centerLeft, // محاذاة النصوص إلى اليسار
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      Textclass.Log_in,  // استدعاء النص من كلاس Textclass
+                      style: TextStyles.Bold30,  // استدعاء التنسيق من كلاس TextStyles
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      Textclass.Please,  // استدعاء النص من كلاس Textclass
+                      style: TextStyles.normal18,  // استدعاء التنسيق من كلاس TextStyles
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: [
+                  // حقل إدخال البريد الإلكتروني
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: Colorclass.gradient,  // استدعاء التدرج من كلاس Colorclass
+                      borderRadius: BorderRadius.circular(30), // زوايا الحواف
+                    ),
+                    padding: const EdgeInsets.all(5), // سمك الحواف
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colorclass.white,  // استدعاء اللون من كلاس Colorclass
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: Textclass.Email,  // استدعاء النص من كلاس Textclass
+                          hintStyle: TextStyles.normal16,  // استدعاء التنسيق من كلاس TextStyles
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none, // بدون حدود إضافية
+                          ),
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 20), // مسافة داخلية
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // حقل إدخال كلمة المرور
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: Colorclass.gradient,  // استدعاء التدرج من كلاس Colorclass
+                      borderRadius: BorderRadius.circular(30), // زوايا الحواف
+                    ),
+                    padding: const EdgeInsets.all(5), // سمك الحواف
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colorclass.white,  // استدعاء اللون من كلاس Colorclass
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: Textclass.Password,  // استدعاء النص من كلاس Textclass
+                          hintStyle: TextStyles.normal16,  // استدعاء التنسيق من كلاس TextStyles
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none, // بدون حدود إضافية
+                          ),
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 20), // مسافة داخلية
+                          suffixIcon: Icon(
+                            Icons.visibility,
+                            color: Colorclass.brown,  // استدعاء اللون من كلاس Colorclass
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      Textclass.ForgotPassword,  // استدعاء النص من كلاس Textclass
+                      style: TextStyles.normal18.copyWith(
+                        color: Colorclass.dustyPink,  // استدعاء اللون من كلاس Colorclass
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  // زر تسجيل الدخول
+                  Align(
+                    alignment: Alignment.centerRight, // محاذاة الزر لليمين
+                    child: Container(
+                      width: 150,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        gradient: Colorclass.gradient,  // استدعاء التدرج من كلاس Colorclass
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: Text(
+                          Textclass.Login,  // استدعاء النص من كلاس Textclass
+                          style: TextStyles.Bold18.copyWith(
+                            color: Colorclass.white,  // استدعاء اللون من كلاس Colorclass
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 80),
+                  // رابط إنشاء حساب جديد
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        Textclass.DontHave,  // استدعاء النص من كلاس Textclass
+                        style: TextStyles.normal18,  // استدعاء التنسيق من كلاس TextStyles
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          Textclass.CreateAccount,  // استدعاء النص من كلاس Textclass
+                          style: TextStyles.normal18.copyWith(
+                            color: Colorclass.dustyPink,  // استدعاء اللون من كلاس Colorclass
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
