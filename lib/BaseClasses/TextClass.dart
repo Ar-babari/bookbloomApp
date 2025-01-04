f706635660af8f1cbd77c54793de02bb46965016
BluseSara <sara.alluhaibi@outlook.com>
2025-01-04 19:25:29 +0300

    .

--- lib/BaseClasses/TextClass.dart
+++ lib/BaseClasses/TextClass.dart
@@ -1,98 +1,132 @@
 class Textclass {
   //splach
 
-  static const Welcome ="Welcome to "; // SplachClass , Size : Bold30 , Color : brown
-  static const BookBloom ="BookBloom "; // SplachClass , Size : Bold30 , Color : dustyPink
-  static const Flourish ="Where Stories Flourish!"; // SplachClass , Size : Bold30 , Color : brown
-  static const Explore ="Explore a World of Free Reading and Writing, Where Every Story is Just a Page Away."; // SplachClass , Size : normal18 , Color : grey
-  static const LetsGo ="Let’s Go "; // SplachClass , Size : Bold18 , Color : white
+  static const Welcome =
+      "Welcome to "; // SplachClass , Size : Bold30 , Color : brown
+  static const BookBloom =
+      "BookBloom "; // SplachClass , Size : Bold30 , Color : dustyPink
+  static const Flourish =
+      "Where Stories Flourish!"; // SplachClass , Size : Bold30 , Color : brown
+  static const Explore =
+      "Explore a World of Free Reading and Writing, Where Every Story is Just a Page Away."; // SplachClass , Size : normal18 , Color : grey
+  static const LetsGo =
+      "Let’s Go "; // SplachClass , Size : Bold18 , Color : white
 
   // login - sign up
 
   static const Log_in = "Log in"; // LoginClass , Size : Bold30 , Color : brown
-  static const Please ="Please Sign In To Continue"; // LoginClass , Size : normal18 , Color : grey
-  static const Email = "Email"; // LoginClass - sign , Size : normal16 , Color : grey
-  static const Password = "Password"; // LoginClass - sign , Size : normal16 , Color : grey
-  static const ForgotPassword ="Forgot Password?"; // LoginClass , Size : normal18 , Color : dustyPink
+  static const Please =
+      "Please Sign In To Continue"; // LoginClass , Size : normal18 , Color : grey
+  static const Email =
+      "Email"; // LoginClass - sign , Size : normal16 , Color : grey
+  static const Password =
+      "Password"; // LoginClass - sign , Size : normal16 , Color : grey
+  static const ForgotPassword =
+      "Forgot Password?"; // LoginClass , Size : normal18 , Color : dustyPink
   static const Login = "Login"; // LoginClass , Size : Bold18 , Color : white
-  static const DontHave ="Don’t Have An Account? "; // LoginClass , Size : Bold18 , Color : brown
-  static const CreateAccount = "Create Account"; // LoginClass , Size : Bold18 , Color : dustyPink
- 
+  static const DontHave =
+      "Don’t Have An Account? "; // LoginClass , Size : Bold18 , Color : brown
+  static const CreateAccount =
+      "Create Account"; // LoginClass , Size : Bold18 , Color : dustyPink
+
   // sign up
- 
-  static const Createyaccount  = "Create your account"; // SignUp , Size : Bold30 , Color : browm
-  static const displayname  = "display name"; // SignUp , Size : normal16 , Color : grey
-  static const Username  = "User name "; // SignUp , Size : normal16 , Color : grey
-  static const Getstarted = "Get started !"; // SignUp , Size : Bold18 , Color : white
- 
+
+  static const Createyaccount =
+      "Create your account"; // SignUp , Size : Bold30 , Color : browm
+  static const displayname =
+      "display name"; // SignUp , Size : normal16 , Color : grey
+  static const Username =
+      "User name "; // SignUp , Size : normal16 , Color : grey
+  static const Getstarted =
+      "Get started !"; // SignUp , Size : Bold18 , Color : white
 
   // Home Page
 
- static const WhatDo = "What do you want to read today?"; // Home Page , Size : Bold30 , Color : white
- static const Novel = "Novel"; // Home Page , Size : normal16 , Color : grey
- static const Selflove = "Self-love"; // Home Page , Size : normal16 , Color : grey
- static const Science = "Science"; // Home Page , Size : normal16 , Color : grey
- static const Romance = "Romance"; // Home Page , Size : normal16 , Color : grey
- static const Tragedy = "Tragedy"; // Home Page , Size : normal16 , Color : grey
-
+  static const WhatDo =
+      "What do you want to read today?"; // Home Page , Size : Bold30 , Color : white
+  static const Novel = "Novel"; // Home Page , Size : normal16 , Color : grey
+  static const Selflove =
+      "Self-love"; // Home Page , Size : normal16 , Color : grey
+  static const Science =
+      "Science"; // Home Page , Size : normal16 , Color : grey
+  static const Romance =
+      "Romance"; // Home Page , Size : normal16 , Color : grey
+  static const Tragedy =
+      "Tragedy"; // Home Page , Size : normal16 , Color : grey
 
   // ReadBook
 
-  static const StartReading = "Start Reading"; // ReadBook , Size : Bold18 , Color : white
+  static const StartReading =
+      "Start Reading"; // ReadBook , Size : Bold18 , Color : white
 
   // profile
-  static const Myprofile  = "My profile ";   // profile , Size : Bold30 , Color : brown
-  static const Book  = "Book ";   // profile , Size : normal16 , Color : grey
-  static const Readers  = "Readers ";   // profile , Size : normal16 , Color : grey
-  static const MyBooks  = "My Books ";   // profile , Size : Bold18 , Color : brown
-  static const MyPicks  = "My Picks ";   // profile , Size : Bold18 , Color : brown
+  static const Myprofile =
+      "My profile "; // profile , Size : Bold30 , Color : brown
+  static const Book = "Book "; // profile , Size : normal16 , Color : grey
+  static const Readers = "Readers "; // profile , Size : normal16 , Color : grey
+  static const MyBooks = "My Books "; // profile , Size : Bold18 , Color : brown
+  static const MyPicks = "My Picks "; // profile , Size : Bold18 , Color : brown
 
+  // Settings
+  static const Settings =
+      "Settings "; // Settings  , Size : Bold30 , Color : brown
+  static const Mode = "Mode  "; // Settings  , Size : normal18 , Color : grey
+  static const Logout = "Log out "; // Settings  , Size : Bold18 , Color : white
+  static const DeleteYourAccount =
+      "Delete Your Account "; // Settings  , Size : Bold18 , Color : Red
 
+  static const parttitle = " part title "; // Size : normal18 , Color : grey
+  static const StartW =
+      " start writing your story"; // Size : normal16 , Color : grey
 
- // Settings 
-  static const Settings = "Settings ";   // Settings  , Size : Bold30 , Color : brown
-  static const Mode = "Mode  ";   // Settings  , Size : normal18 , Color : grey
-  static const Logout = "Log out ";   // Settings  , Size : Bold18 , Color : white
-  static const DeleteYourAccount = "Delete Your Account ";   // Settings  , Size : Bold18 , Color : Red
+  static const Editstory =
+      "Edit story"; // profile , Size : Bold18 , Color : brown
+  static const Save = "save"; // profile , Size : Bold18 , Color : brown
+  static const AddACover =
+      "Add a cover"; // profile , Size : Bold16 , Color : brown
+  static const Title = "Title "; // profile , Size : Normal18 , Color : brown
+  static const Description =
+      "Description "; // profile , Size : Normal18 , Color : brown
+  static const Category =
+      "Category "; // profile , Size : Normal18 , Color : brown
+  static const Tags = "Tags "; // profile , Size : Normal18 , Color : brown
+  static const StoryLanguage =
+      "Story Language "; // profile , Size : Normal18 , Color : brown
+  static const English = "English "; // profile , Size : Normal18 , Color : grey
+  static const Copyright =
+      "Copyright "; // profile , Size : Normal18 , Color : brown
+  static const AllRightsReserved =
+      "All Rights Reserved    "; // profile , Size : Normal18 , Color : grey
+  static const Mature = "Mature "; // profile , Size : Normal18 , Color : brown
+  static const Completed =
+      "Completed "; // profile , Size : Normal18 , Color : brown
+  static const HintTags =
+      "Adding tags helps readers find your story!"; // profile , Size : hint14 , Color : grey
+  static const HintMature =
+      "Your story is approptiate for all audiences.The Wattpad community has the abilty to rate your story Mature For mor info, check the content Guidelines under Setting."; // profile , Size : hint14, Color : grey
 
+  static const ForgetPass = "Forgot Password"; // size : bold30 , color : brown
+  static const reset =
+      "enter your email account to reset password"; // size : normal18 color :grey
+  static const Continue = "Continue"; //size : bold18 , color : white
 
-  static const  parttitle = " part title ";    // Size : normal18 , Color : grey
-  static const  StartW = " start writing your story";    // Size : normal16 , Color : grey
-
-
-  static const Editstory  = "Edit story";// profile , Size : Bold18 , Color : brown
-  static const Save  = "save";// profile , Size : Bold18 , Color : brown
-  static const AddACover  = "Add a cover";   // profile , Size : Bold16 , Color : brown
-  static const Title  = "Title ";   // profile , Size : Normal18 , Color : brown
-  static const Description  = "Description ";   // profile , Size : Normal18 , Color : brown
-  static const Category  = "Category ";   // profile , Size : Normal18 , Color : brown
-  static const Tags  = "Tags ";   // profile , Size : Normal18 , Color : brown
-  static const StoryLanguage  = "Story Language ";   // profile , Size : Normal18 , Color : brown
-  static const English   = "English ";   // profile , Size : Normal18 , Color : grey
-  static const Copyright  = "Copyright ";   // profile , Size : Normal18 , Color : brown
-  static const AllRightsReserved     = "All Rights Reserved    ";   // profile , Size : Normal18 , Color : grey
-  static const Mature  = "Mature ";   // profile , Size : Normal18 , Color : brown
-  static const Completed  = "Completed ";   // profile , Size : Normal18 , Color : brown
-  static const HintTags  = "Adding tags helps readers find your story!";   // profile , Size : hint14 , Color : grey
-  static const HintMature  = "Your story is approptiate for all audiences.The Wattpad community has the abilty to rate your story Mature For mor info, check the content Guidelines under Setting.";   // profile , Size : hint14, Color : grey
-
-  static const ForgetPass = "Forgot Password" ; // size : bold30 , color : brown
-  static const reset = "enter your email account to reset password"; // size : normal18 color :grey
-  static const Continue ="Continue"; //size : bold18 , color : white
-
-
-  static const Checkyouremail = "Check your email"; //size : bold30 , color : brown
-  static const WeHaveSent ="We have sent an instructions to recover your password to your email"; // size : n18 , color :grey
-  static const erroelogin = "your password or your email is not correct"; //size : bold18 or 30 , color : brown
+  static const Checkyouremail =
+      "Check your email"; //size : bold30 , color : brown
+  static const WeHaveSent =
+      "We have sent an instructions to recover your password to your email"; // size : n18 , color :grey
+  static const erroelogin =
+      "your password or your email is not correct"; //size : bold18 or 30 , color : brown
   static const ok = "ok"; //size : bold18 , color white
   static const Done = "Done"; // size :bold30 , color :white
   static const mybookshelf = "My book shelf"; // size :bold30 , color :brown
-  static const currentlyreading = "currently reading"; // size :bold18 , color :brown
+  static const currentlyreading =
+      "currently reading"; // size :bold18 , color :brown
   static const readinglater = "reading later"; // size :bold18 , color :brown
   static const myfavourite = "my favourite"; // size :bold18 , color :brown
   static const shelfName = "Shelf name"; // size : normal18 , color grey
-  static const CreateShelf = "Create Shelf"; // size :bold18 , color :whiet
-
-
-
+  static const CreateShelf = "Create Shelf"; // size :bold18 , color :white
+  static const author = "author"; //Size : normal16 , Color : grey
+  static const Both = "Both "; //Size : normal16 , Color : grey
+  static const Title1 = "Title"; //Size : normal16 , Color : grey
+  static const Author = "Author"; //Size : normal16 , Color : grey
 }
