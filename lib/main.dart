242dee0eabeea3f5b192f19229a9a97004cb0138
Ar-babari <aab911415@gmail.com>
2025-01-14 11:32:32 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bookbloom/HomeScreen.dart';
 import 'package:bookbloom/LoginScreen.dart';
+import 'package:bookbloom/SignUpScreen.dart';
 import 'package:bookbloom/readbookScreen.dart';
+import 'package:bookbloom/resetpassword.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Resetpassword(),
     );
   }
 }
